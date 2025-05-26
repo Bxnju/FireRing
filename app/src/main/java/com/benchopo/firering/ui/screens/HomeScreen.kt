9244900dff5c2f6b10052100e2c49295c872e24b
Juan Andres Porras <juanandresporras@gmail.com>
2025-05-26 17:48:26 -0500

    fix: Home screen params check

--- app/src/main/java/com/benchopo/firering/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/benchopo/firering/ui/screens/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.benchopo.firering.ui.screens
 
 import androidx.compose.foundation.Image
-import com.benchopo.firering.R
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -11,32 +10,31 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.benchopo.firering.R
 import com.benchopo.firering.navigation.Routes
 import com.benchopo.firering.viewmodel.GameViewModel
 
 @Composable
-fun HomeScreen(navController: NavController) {
-    Column(
-            modifier = Modifier.fillMaxSize().padding(24.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // Title
-        Row {
-            Text(
-                "Ring of Fire ",
-                style = MaterialTheme.typography.headlineLarge,
-                textAlign = TextAlign.Center
-            )
+fun HomeScreen(navController: NavController, gameViewModel: GameViewModel) {
+        Column(
+                modifier = Modifier.fillMaxSize().padding(24.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+                // Title
+                Row {
+                        Text(
+                                "Ring of Fire ",
+                                style = MaterialTheme.typography.headlineLarge,
+                                textAlign = TextAlign.Center
+                        )
 
-            Image(
-                painter = painterResource(id = R.drawable.ic_beer),
-                contentDescription = "Beer Icon",
-                modifier = Modifier.size(48.dp)
-            )
-
-        }
-
+                        Image(
+                                painter = painterResource(id = R.drawable.ic_beer),
+                                contentDescription = "Beer Icon",
+                                modifier = Modifier.size(48.dp)
+                        )
+                }
 
                 Spacer(modifier = Modifier.height(16.dp))
 
