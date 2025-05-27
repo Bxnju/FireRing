b812c5dc516674092dc968883d3d056146c4d093
Bxnju <andres.benjumea@upb.edu.co>
2025-05-27 00:25:06 -0500

    feat: Update color scheme for FireRing theme
    This commit updates the color scheme for both dark and light themes in the FireRing application.
    
    - **Dark Theme:**
        - `primary` changed from `Purple80` to `DarkOrange`.
        - `secondary` changed from `PurpleGrey80` to `Firebrick`.
        - `tertiary` changed from `Pink80` to `Tan`.
    - **Light Theme:**
        - `primary` changed from `Purple40` to `DarkOrange`.
        - `secondary` changed from `PurpleGrey40` to `Firebrick`.
        - `tertiary` changed from `Pink40` to `Tan`.
    - The `background` for both themes remains `Black`.

--- app/src/main/java/com/benchopo/firering/ui/theme/Theme.kt
+++ app/src/main/java/com/benchopo/firering/ui/theme/Theme.kt
@@ -12,16 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = DarkOrange,
+    secondary = Firebrick,
+    tertiary = Tan,
     background = Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = DarkOrange,
+    secondary = Firebrick,
+    tertiary = Tan,
     background = Black
 
     /* Other default colors to override
