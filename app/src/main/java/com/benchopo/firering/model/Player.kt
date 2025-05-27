6748b6bf798906aa6cd7c43f751a23fee4e572b5
Juan Andres Porras <juanandresporras@gmail.com>
2025-05-27 01:55:40 -0500

    fix: Modified the entity mapping

--- app/src/main/java/com/benchopo/firering/model/Player.kt
+++ app/src/main/java/com/benchopo/firering/model/Player.kt
@@ -1,14 +1,27 @@
 package com.benchopo.firering.model
 
+import com.google.firebase.database.PropertyName
+
 data class Player(
         val id: String = "",
         val name: String = "",
-        val isHost: Boolean = false,
-        val isGuest: Boolean = true,
+
+        @get:PropertyName("isHost")
+        @set:PropertyName("isHost")
+        var isHost: Boolean = false,
+
+        @get:PropertyName("isGuest")
+        @set:PropertyName("isGuest")
+        var isGuest: Boolean = true,
+
         val selectedDrinkId: String? = null,
         val drinkCount: Int = 0,
         val mateIds: List<String> = emptyList(),
-        val isConnected: Boolean = true,
+
+        @get:PropertyName("isConnected")
+        @set:PropertyName("isConnected")
+        var isConnected: Boolean = true,
+
         val lastActiveTimestamp: Long = System.currentTimeMillis(),
         val customRuleIds: List<String> = emptyList(),
         val turnIndex: Int? = null
