e8a6717c3af0fd58ab69ad7e43b712de9746438a
Bxnju <andres.benjumea@upb.edu.co>
2025-05-27 21:18:52 -0500

    feat: Configure splash screen and update dependencies
    This commit updates the application theme to support a splash screen and modifies dependencies.
    
    - **`AndroidManifest.xml`**: Removed the `android:label` attribute from the `MainActivity` definition.
    - **`app/build.gradle.kts`**:
        - Added `androidx.compose.material3:material3:1.2.1` and `com.google.android.material:material:1.11.0` dependencies.
        - Reordered `splashscreen` and `firebase-bom` dependencies.
    - **`app/src/main/res/values/themes.xml`**:
        - In the `Theme.App.Starting` style, added:
            - `<item name="android:windowNoTitle">true</item>`
            - `<item name="android:windowFullscreen">true</item>`
        to make the splash screen full screen and without a title bar.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,20 +28,22 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,16 +51,19 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3) // Compose M3
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.navigation.compose)
     implementation(libs.firebase.database)
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation(libs.splashscreen)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    implementation(libs.splashscreen)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
 }
